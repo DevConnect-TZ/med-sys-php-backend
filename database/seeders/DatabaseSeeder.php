3d23bbe0ac43955d88f07d13a74826a936b998b6
sirtheprogrammer <sirtheprogrammer@gmail.com>
2026-04-15 15:43:30 +0300

    add the codebase

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,69 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $demoAccounts = [
+            [
+                'name' => 'Administrator',
+                'email' => 'admin@hospital.com',
+                'password' => 'AdminPass123!@',
+                'role' => 'admin',
+                'phone' => '+1234567890',
+            ],
+            [
+                'name' => 'Dr. John Doe',
+                'email' => 'doctor@hospital.com',
+                'password' => 'DemoPass123!@',
+                'role' => 'doctor',
+                'phone' => '+1234567891',
+            ],
+            [
+                'name' => 'Nurse Jane',
+                'email' => 'nurse@hospital.com',
+                'password' => 'DemoPass123!@',
+                'role' => 'nurse',
+                'phone' => '+1234567892',
+            ],
+            [
+                'name' => 'Receptionist Mike',
+                'email' => 'receptionist@hospital.com',
+                'password' => 'DemoPass123!@',
+                'role' => 'receptionist',
+                'phone' => '+1234567893',
+            ],
+            [
+                'name' => 'Cashier Sarah',
+                'email' => 'cashier@hospital.com',
+                'password' => 'DemoPass123!@',
+                'role' => 'cashier',
+                'phone' => '+1234567894',
+            ],
+            [
+                'name' => 'Lab Tech Alex',
+                'email' => 'lab@hospital.com',
+                'password' => 'DemoPass123!@',
+                'role' => 'lab_technician',
+                'phone' => '+1234567895',
+            ],
+            [
+                'name' => 'Pharmacist Emma',
+                'email' => 'pharmacist@hospital.com',
+                'password' => 'DemoPass123!@',
+                'role' => 'pharmacist',
+                'phone' => '+1234567896',
+            ],
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($demoAccounts as $account) {
+            User::updateOrCreate(
+                ['email' => $account['email']],
+                [
+                    'name' => $account['name'],
+                    'password' => Hash::make($account['password']),
+                    'role' => $account['role'],
+                    'phone' => $account['phone'],
+                    'is_active' => true,
+                ]
+            );
+        }
     }
 }
